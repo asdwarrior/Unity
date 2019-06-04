b1a4c4bf93a574979c15d196296d3fa2cbc10dbe
asdwarrior <50559985+asdwarrior@users.noreply.github.com>
2019-06-04 09:47:31 +0900

    Add files via upload

---
+++ EnemyController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EnemyController : MonoBehaviour
+{  
+    //AI
+	public Transform Player;
+	public NavMeshAgent Agent;
+    //Score
+    static int score;
+    public Text scoretext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Agent.SetDestination(Player.transform.position);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Bullet")
+        {   
+            score = score + 1;
+            scoretext.text = "Score:  " + score;
+            Destroy(gameObject);
+            Debug.Log(score);
+            if(score>=4)
+            {
+                SceneManager.LoadScene(0);
+                score=0;
+            }
+
+        }
+
+    }
+}
---
+++ Move.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Move : MonoBehaviour
+{
+	Rigidbody rb;
+    //HP
+    public int hp;
+    public Text hpt;
+	//Пуля
+	public GameObject bullet;
+    GameObject bulletClone;
+    Rigidbody rbBullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        hp = 100;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        rb.AddForce(transform.forward * moveVertical * 40f);
+        transform.Rotate(0, moveHorizontal*5f, 0);
+        rb.AddForce(moveHorizontal * transform.right * 40f);
+
+        if(Input.GetButtonDown("Fire1")){
+            bulletClone = Instantiate(bullet, new Vector3(transform.position.x, transform.position.y, transform.position.z + 1f), Quaternion.identity);
+            rbBullet = bulletClone.GetComponent<Rigidbody>();
+            rbBullet.AddForce(transform.forward * 500f);
+    }
+
+        
+}
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {   
+            hp = hp - 1;
+            hpt.text = "HP: " + hp;
+            Debug.Log(hp);
+
+            if(hp<=0)
+            {
+                SceneManager.LoadScene(1);
+                hp=100;
+            }
+        }  
+    }
+
+    }
